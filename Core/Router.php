9fa4d465de3890c762732c8adea69fbef22544e0
mason.hows11 <mason.hows11@gmail.com>
2023-04-29 19:09:11 +0430

    route system updated

--- Core/Router.php
+++ Core/Router.php
@@ -1,8 +1,52 @@
 <?php
 
 
-class Router{
+class Router
+{
+
+    protected $routes = [];
 
 
-    
+    protected $params = [];
+
+    /**
+     * @param $route
+     * @param $params
+     * add a route to the routing table
+     * route is route
+     * params is controller & actions or etc
+     */
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+
+    /*
+     * get all the  routes from routing table
+     * return array []
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+
+
+    public function match_route($url)
+    {
+
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,6 +1,23 @@
 <?php
 
 
-
-
 require '../Core/Router.php';
+
+$router = new Router();
+
+
+// add the routes
+$router->add('/', ['controller' => 'Home', 'action' => 'index']);
+$router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('/posts/create', ['controller' => 'Posts', 'action' => 'create']);
+
+
+// match the requested route
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+if ($router->match_route($url)) {
+    echo '<pre/>';
+    var_dump($router->getRoutes());
+    echo '<pre/>';
+} else {
+    echo "no route found for url {$url}";
+}
